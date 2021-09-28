e797ef6aac64f454c723416b6e6188dcf1257cc8
Aline Hermosillo Sada <aline.hermosillo@udem.edu>
2021-09-28 16:45:36 -0500

    Agrego otro archivo a mi carpeta proyectos

---
+++ prueba.py
@@ -0,0 +1,5 @@
+#Inicio
+'''este es un comentario largo'''
+edad=int(input("Dame la edad: "))
+print("Hola alumno de Programación"+"\nTienes "+str(edad)+ " años")
+#Fin
---
+++ prueba2.py
@@ -0,0 +1,8 @@
+#Inicio
+suma=0
+for i in range(1,6): #rango es de 1 a 6, 5 veces en total. i automáticamente es 1=1+1
+  num=int(input("Dame un número: "))
+  suma+=num # suma=suma+num
+#fin for
+print("La suma es: ",suma)
+#Fin
