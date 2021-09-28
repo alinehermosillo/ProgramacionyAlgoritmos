db16ed9246dbf5b4adfcfe67046d0e1c9bf43ce9
Aline Hermosillo Sada <aline.hermosillo@udem.edu>
2021-09-28 17:19:51 -0500

    Agregar archivo 3

---
+++ prueba3.py
@@ -0,0 +1,9 @@
+#Inicio
+num=0
+print("Este programa solicita números positivos.")
+print("Cuando ya no quieras ingresar números, ingresa un número negativo")
+while num>=0:
+    num=int(input("Dame un número: "))
+    #indentación de instrucciones del while
+#Fin while
+#Fin
